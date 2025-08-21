3449bcab2f4d4774fcfc71eee6c251bf957b9ed3
WeidosOddities <michaeljbug0602@gmail.com>
2025-08-20 19:12:36 -0600

    regen data

--- src/main/java/com/weido/create_bb/data/BuilderTransformers.java
+++ src/main/java/com/weido/create_bb/data/BuilderTransformers.java
@@ -1,5 +1,6 @@
 package com.weido.create_bb.data;
 
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.data.BlockStateGen;
 import com.simibubi.create.foundation.data.SharedProperties;
@@ -8,19 +9,22 @@ import com.tterrag.registrate.util.nullness.NonNullUnaryOperator;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import com.weido.create_bb.blocks.BBBogieBlock;
+import net.minecraft.world.level.material.MapColor;
 
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 
 public class BuilderTransformers {
 
-    public static <B extends BBBogieBlock, P> NonNullUnaryOperator<BlockBuilder<B, P>> sharedBogey() {
+    public static <B extends BBBogieBlock, P> NonNullUnaryOperator<BlockBuilder<B, P>> bogie() {
         return b -> b.initialProperties(SharedProperties::softMetal)
             .properties(p -> p.sound(SoundType.NETHERITE_BLOCK))
             .properties(BlockBehaviour.Properties::noOcclusion)
             .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+            .properties(p -> p.mapColor(MapColor.PODZOL))
             .transform(pickaxeOnly())
             .blockstate((c, p) -> BlockStateGen.horizontalAxisBlock(c, p, s -> p.models()
             .getExistingFile(p.modLoc("block/bogie/top"))))
-            .tag(AllTags.AllBlockTags.SAFE_NBT.tag);
+            .loot((p, l) -> p.dropOther(l, AllBlocks.RAILWAY_CASING.get()))
+            .tag(AllTags.AllBlockTags.WRENCH_PICKUP.tag);
     }
 }
