b2207cb7ce661497dce3284aa4bfe1440e311227
WeidosOddities <michaeljbug0602@gmail.com>
2025-08-25 23:57:48 -0600

    This might be the cause of the weird crash

--- src/main/java/com/weido/create_bb/BlocksBogiesClient.java
+++ src/main/java/com/weido/create_bb/BlocksBogiesClient.java
@@ -2,17 +2,14 @@ package com.weido.create_bb;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.createmod.catnip.render.SuperByteBufferCache;
-import net.createmod.catnip.render.CachedBuffers;
 import com.weido.create_bb.registry.BogiePartials;
 
 public class BlocksBogiesClient {
     public static void onCtorClient(IEventBus modEventBus) {
         modEventBus.addListener(BlocksBogiesClient::clientInit);
     }
+
     public static void clientInit(final FMLCommonSetupEvent event) {
-        SuperByteBufferCache.getInstance().registerCompartment(CachedBuffers.PARTIAL);
-        SuperByteBufferCache.getInstance().registerCompartment(CachedBuffers.DIRECTIONAL_PARTIAL);
         BogiePartials.init();
     }
 }
