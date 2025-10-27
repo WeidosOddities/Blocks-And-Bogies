28ce85875d0bcfd4a79a6785a43fc780a84e005b
WeidosOddities <michaeljbug0602@gmail.com>
2025-10-27 00:24:19 -0600

    Forgot this

--- src/main/java/com/weido/create_bb/data/compat/steamnrails/MenuSwitchButton.java
+++ src/main/java/com/weido/create_bb/data/compat/steamnrails/MenuSwitchButton.java
@@ -3,7 +3,6 @@ package com.weido.create_bb.data.compat.steamnrails;
 import com.railwayteam.railways.content.bogey_menu.BogeyMenuScreen;
 import com.simibubi.create.foundation.gui.AllIcons;
 import com.simibubi.create.foundation.gui.widget.IconButton;
-import com.weido.create_bb.data.accessors.RailwayScreenAccessor;
 import net.createmod.catnip.gui.ScreenOpener;
 import net.createmod.catnip.gui.widget.AbstractSimiWidget;
 import net.minecraft.core.BlockPos;
@@ -24,7 +23,7 @@ public class MenuSwitchButton {
         IconButton menuSwitchButton = new IconButton(x, y, AllIcons.I_DICE)
                 .withCallback(() -> {
                     BogeyMenuScreen screen = new BogeyMenuScreen();
-//                    RailwayScreenAccessor.setTargetPos(targetPos);
+                    setTargetPos(targetPos);
                     ScreenOpener.open(screen);
                     onMenuSwitch.run();
                 });
