79f9baeabedf7837443a5618d4ccaf86214a77fe
WeidosOddities <michaeljbug0602@gmail.com>
2026-08-24 00:51:21 -0600

    Fixed lighting issues with the XL10S - Gearless when flywheel backend is enabled and the bogie is assembled Fixed a rotation issue with the L6S - Gearless when flywheel backend is enabled and the bogie is assembled
    1.20.1

--- src/main/java/com/weido/create_bb/visuals/gearless/TripleAxleShortVisual.java
+++ src/main/java/com/weido/create_bb/visuals/gearless/TripleAxleShortVisual.java
@@ -142,6 +142,7 @@ public class TripleAxleShortVisual extends BlocksBogiesBogieVisual {
             belt.offset(0, BELT_RADIUS_IN_UV_SPACE * Mth.DEG_TO_RAD * wheelAngle)
                     .setTransform(poseStack)
                     .scale(1 - 1 / 512f)
+                    .rotateYDegrees(forwards ? 180 : 0)
                     .setChanged();
 
             for (int i = 0; i < 2; i++) {
